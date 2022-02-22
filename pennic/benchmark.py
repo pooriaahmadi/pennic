f6d3fcebb2597cea9b878484363f2c8f7b553411
pooriaahmadi <pooriababak444@gmail.com>
2022-02-21 19:50:11 -0500

    added miner_private_key to benchmark

--- pennic/benchmark.py
+++ pennic/benchmark.py
@@ -1,7 +1,11 @@
 from Blockchain import Block
 import time
+from Crypto.PublicKey import RSA
 do_cycles = 5
 final_results = []
+private = None
+with open("private.pem", "rb") as private_file:
+    private = RSA.import_key(private_file.read())
 for i in range(do_cycles):
     block = Block(index=0, timestamp=time.time(), hardness=5)
     hashesh_per_cycle = increase_value = 500
@@ -10,7 +14,7 @@ for i in range(do_cycles):
     while not is_done:
         tic = time.time()
         block.calculate_correct_hash_multiprocess(
-            hashes_per_cycle=hashesh_per_cycle)
+            hashes_per_cycle=hashesh_per_cycle, miner_private_key=private)
         toc = time.time()
         if len(results) == 0 or (toc - tic) < results[-1][0]:
             results.append([toc - tic, hashesh_per_cycle])
