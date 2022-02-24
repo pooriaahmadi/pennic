af46a87bbaf7453e5ba9e4f04e3751031195b373
pooriaahmadi <pooriababak444@gmail.com>
2022-02-24 12:50:13 -0500

    rewrote the node reading method

--- pennic/node.py
+++ pennic/node.py
@@ -2,8 +2,11 @@ from dotenv import load_dotenv
 from Node import Node
 from Crypto.PublicKey import RSA
 from Blockchain import Blockchain
+from typing import List
+from Blockchain import Address
 import os
 import json
+
 
 load_dotenv()
 
@@ -13,18 +16,22 @@ port = int(os.getenv("PORT"))
 private = None
 with open("private.pem", "rb") as private_file:
     private = RSA.import_key(private_file.read())
-nodes = []
+nodes: List[Address] = []
 try:
     with open("recent_nodes.json", "r", encoding="utf-8") as nodes_file:
-        nodes = json.loads(nodes_file.read())
-        if nodes == []:
+        raw_nodes = json.loads(nodes_file.read())
+        for node in raw_nodes:
+            nodes.append(Address(node["ip"], node["port"]))
+
+        if not len(nodes):
             input_node = input("Please enter a node to start")
             if input_node:
-                nodes.append(input_node)
+                nodes.append(Address(input_node))
 except:
     input_node = input("Please enter a node to start")
     if input_node:
-        nodes.append(input_node)
+        nodes.append(Address(input_node))
+
 chain = Blockchain()
 chain.load_database()
 
